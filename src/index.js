86a8614c7ee248e3ba08e41e4a56ac6c2db6af20
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-17 18:43:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -60,10 +60,7 @@ async function connectDB() {
   if (isConnected) return;
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const db = await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = db.connections[0].readyState;
     console.log('MongoDB Connected');
