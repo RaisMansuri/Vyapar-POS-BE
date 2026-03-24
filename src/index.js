3b30723f4619caebfb26bb35f219b6ac6184a22e
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-24 18:51:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const app = express();
 ======================= */
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Skip-Loader']
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization', 'X-Skip-Loader']
 }));
 
 
