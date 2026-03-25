908dcaa563ec63662f8f2e211b2574fb8c100846
Rais Ahmad <raismansuri74059@gmail.com>
2026-03-25 10:46:01 +0530

    Update swagger.config.js

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -17,6 +17,10 @@ const options = {
         url: 'http://localhost:5000',
         description: 'Development server',
       },
+      {
+        url: 'https://vyapar-pos-be-git-development-raismansuri74059-1745s-projects.vercel.app',
+        description: 'Production server',
+      },
     ],
     components: {
       securitySchemes: {
